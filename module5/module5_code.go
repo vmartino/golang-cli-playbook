7c5eb55f19e98cf0443937d9ba76c189cb739313
Vincenzo Edoardo Martino <vincenzoedoardo.martino@gmail.com>
2020-12-05 20:05:58 -0500

    Completed module

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,6 +1,6 @@
 package module5
 
-/*
+
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
@@ -10,4 +10,4 @@ func GetExampleDotCom() {
 
 	defer resp.Body.Close()
 }
-*/
+
