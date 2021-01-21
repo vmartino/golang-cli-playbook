0ac878e6a508ce87959a31a2af68921ebab01313
Vincenzo Edoardo Martino <vincenzoedoardo.martino@gmail.com>
2021-01-20 22:57:11 -0500

    Fix as suggested on pluralsight project discussion

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -7,10 +7,10 @@ import (
 
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 func GetExampleDotCom() {
-	resp, err := http.Get("http://example.com/")
-	if err != nil {
-		fmt.Println("something went wrong")
-	}
+	//	resp, err := http.Get("http://example.com/")
+	//	if err != nil {
+	//		fmt.Println("something went wrong")
+	//	}
 
-	defer resp.Body.Close()
+	//	defer resp.Body.Close()
 }
